91a2d0fd50ba6bbc59a515a446fc0e031b6afe08
sidanish <sidanish458@gmail.com>
2026-05-08 10:55:06 +0000

    Deploying to gh-pages from @ sidanish/sidanish.github.io@2d0fd8db3f8b52eb745b78f044b2b1b2579c4f14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-experience",
+          title: "experience",
+          description: "My professional experience and career journey.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/experience/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
