448932c1698680ee6d71c31a572e8f639a94a97d
sidanish <sidanish458@gmail.com>
2026-05-10 09:48:58 +0000

    Deploying to gh-pages from @ sidanish/sidanish.github.io@3754f93d076608d8846964d5f109dec1f8020988 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/experience/";
+            window.location.href = "/cv/";
           },
         },{id: "nav-experience",
           title: "experience",
